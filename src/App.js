4220961d8ae280e2c2bf67b7f7cd62a8e215af73
Tep Thean <tep.thean@DETTHEAN.sabay.office>
2025-02-07 13:10:51 +0700

    test7

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hi it is me about you
+    <div className="app-container">
+      <h1 className="greeting-text">Hi, it is me! What about you?</h1>
+      <p className="description-text">Let's make this page look cool and awesome.</p>
     </div>
   );
 }
